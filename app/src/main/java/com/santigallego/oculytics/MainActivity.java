8c65a17f860b5588ec9083abca6df6e2f38e41d6
Santi Gallego <santigallego9@gmail.com>
2016-08-16 01:07:56 -0500

    switched to seed.sql building instead of manual queries

--- app/src/main/java/com/santigallego/oculytics/MainActivity.java
+++ app/src/main/java/com/santigallego/oculytics/MainActivity.java
@@ -1,11 +1,15 @@
 package com.santigallego.oculytics;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +17,11 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.santigallego.oculytics.SmsOutgoingObserver;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,20 +35,22 @@ public class MainActivity extends AppCompatActivity {
         smsPermissions();
         contactPermissions();
 
+        Intent intent = new Intent(this, SmsOutgoingObserver.class);
+        startService(intent);
+
         try {
             SQLiteDatabase db = this.openOrCreateDatabase(Database.DATABASE_NAME, MainActivity.MODE_PRIVATE, null);
+            Scanner scan = new Scanner(getResources()
+                    .openRawResource(R.raw.seed));
 
-            // UPDATE totals SET received = received + 1
-            String create = "CREATE TABLE totals (sent INTEGER default 0, received INTEGER default 0, created_on DATETIME default current_timestamp, updated_on DATETIME default current_timestamp);";
-            String insert = "INSERT INTO totals (sent, received) VALUES (0, 0);";
-
-            db.execSQL(create);
-            db.execSQL(insert);
-
-            // UPDATE totals SET received = received + 1
-            create = "CREATE TABLE contacts (id INTEGER PRIMARY KEY, number TEXT, sent INTEGER, received INTEGER, streak INTEGER, created_on DATETIME default current_timestamp, updated_on DATETIME default current_timestamp);";
-
-            db.execSQL(create);
+            String query = "";
+            while (scan.hasNextLine()) { // build and execute queries
+                query += scan.nextLine() + "\n";
+                if (query.trim().endsWith(";")) {
+                    db.execSQL(query);
+                    query = "";
+                }
+            }
         } catch (Exception e) {
             Log.d("TABLE_EXISTS", e.toString());
         }
@@ -104,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
         // UPDATE totals SET received = received + 1
         String query = "SELECT received FROM totals;";
+        String totals = "", contacts = "", sms_received = "";
 
         Cursor cr = db.rawQuery(query, null);
 
@@ -111,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
             do {
                 int total = cr.getInt(cr.getColumnIndex("received"));
                 Log.d("TEXT_MESSAGE", total + "");
+                totals = "TOTAL: " + total;
             } while (cr.moveToNext());
             cr.close();
         }
@@ -128,9 +140,32 @@ public class MainActivity extends AppCompatActivity {
                 String created_on = cr.getString(cr.getColumnIndex("created_on"));
                 String updated_on = cr.getString(cr.getColumnIndex("updated_on"));
                 String text = "ID: " + id + ", NUMBER: " + number + ", REC: " + received + ", SENT: " + sent + ", CREATED_ON: " + created_on + ", UPDATED_ON: " + updated_on;
+                contacts += text + "\n\n";
                 Log.d("TEXT_MESSAGE", text);
             } while (cr.moveToNext());
             cr.close();
+        }
+
+        query = "SELECT * FROM sms_received;";
+
+        cr = db.rawQuery(query, null);
+
+        if (cr.moveToFirst()) {
+            do {
+                int id = cr.getInt(cr.getColumnIndex("id"));
+                int contact_id = cr.getInt(cr.getColumnIndex("contact_id"));
+                String received_on = cr.getString(cr.getColumnIndex("received_on"));
+                String text = "ID: " + id + ", CONTACT_ID: " + contact_id + ", RECEIVED_ON: " + received_on;
+                sms_received += text + "\n\n";
+                Log.d("TEXT_MESSAGE", text);
+            } while (cr.moveToNext());
+            cr.close();
+
+            String final_text = "TOTALS\n------------------\n" + totals + "\n\n\n\nCONTACTS\n------------------\n" + contacts + "\n\n\n\nSMS\n------------------\n" + sms_received;
+
+            TextView queryView = (TextView) findViewById(R.id.query_text);
+
+            queryView.setText(final_text);
         }
     }
 }
