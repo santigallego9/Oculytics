1810c5f655d2044bca6821774409bfcdadb1975a
Santi Gallego <santigallego9@gmail.com>
2016-08-16 01:06:49 -0500

    fixed minor logic issues

--- app/src/main/java/com/santigallego/oculytics/Database.java
+++ app/src/main/java/com/santigallego/oculytics/Database.java
@@ -1,15 +1,11 @@
 package com.santigallego.oculytics;
 
-import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-import android.widget.Toast;
 
-import java.util.Scanner;
-
-/**
+/*
  * Created by santigallego on 8/14/16.
  */
 public class Database {
@@ -33,39 +29,46 @@ public class Database {
         }
 
         SQLiteDatabase db = context.openOrCreateDatabase(DATABASE_NAME, MainActivity.MODE_PRIVATE, null);
-
-        String insertQuery = "", updateQuery = "", selectQuery = "";
+        String insertQuery, updateQuery, selectQuery;
 
         // UPDATE totals SET received = received + 1
         updateQuery = "UPDATE totals SET received = received + 1, updated_on = current_timestamp;";
-
         db.execSQL(updateQuery);
 
-        selectQuery = "SELECT number FROM contacts;";
-
+        selectQuery = "SELECT * FROM contacts WHERE number = " + number + ";";
         Cursor cr = db.rawQuery(selectQuery, null);
 
-        boolean checker = false;
+        //boolean checker = false;
         if (cr.moveToFirst()) {
             Log.d("TEXT_MESSAGE", "ENTERED " + number);
             do {
-                if(cr.getString(cr.getColumnIndex("number")).equals(number)) {
-                    Log.d("TEXT_MESSAGE", "EXISTS");
-                    updateQuery = "UPDATE contacts SET received = received + 1, updated_on = current_timestamp WHERE number = " + number;
-                    db.execSQL(updateQuery);
-                    checker = true;
-                }
-            } while (cr.moveToNext());
-            if (!checker) {
-                Log.d("TEXT_MESSAGE", "DOES NOT EXIST");
-                insertQuery = "INSERT INTO contacts (number, sent, received) VALUES (" + number + ", 0, 1);";
+                Log.d("TEXT_MESSAGE", "EXISTS");
+                int id = cr.getInt(cr.getColumnIndex("id"));
+                updateQuery = "UPDATE contacts SET received = received + 1, updated_on = current_timestamp WHERE number = " + number + ";";
+                insertQuery = "INSERT INTO sms_received (contact_id) VALUES (" + id + ");";
+                Log.d("TEXT_MESSAGE", insertQuery);
+                db.execSQL(updateQuery);
                 db.execSQL(insertQuery);
-            }
+
+            } while (cr.moveToNext());
             cr.close();
         } else {
             Log.d("TEXT_MESSAGE", "DOES NOT EXIST");
-            insertQuery = "INSERT INTO contacts (number, sent, received) VALUES (" + number + ", 0, 1);";
+            insertQuery = "INSERT INTO contacts (number, received) VALUES (" + number + ", 1);";
             db.execSQL(insertQuery);
+
+            Cursor c = db.rawQuery(selectQuery, null);
+            //boolean checker = false;
+            if (c.moveToFirst()) {
+                do {
+                    Log.d("TEXT_MESSAGE", "EXISTS");
+                    int id = c.getInt(c.getColumnIndex("id"));
+                    insertQuery = "INSERT INTO sms_received (contact_id) VALUES (" + id + ");";
+                    db.execSQL(insertQuery);
+
+                } while (c.moveToNext());
+                c.close();
+            }
         }
     }
 
@@ -85,38 +88,53 @@ public class Database {
 
         SQLiteDatabase db = context.openOrCreateDatabase(DATABASE_NAME, MainActivity.MODE_PRIVATE, null);
 
-        String insertQuery = "", updateQuery = "", selectQuery = "";
+        String insertQuery, updateQuery, selectQuery;
 
         // UPDATE totals SET received = received + 1
         updateQuery = "UPDATE totals SET sent = sent, updated_on = current_timestamp + 1;";
 
         db.execSQL(updateQuery);
 
-        selectQuery = "SELECT number FROM contacts;";
+        selectQuery = "SELECT * FROM contacts WHERE number = " + number + ";";
 
         Cursor cr = db.rawQuery(selectQuery, null);
 
-        boolean checker = false;
+        //boolean checker = false;
         if (cr.moveToFirst()) {
             Log.d("TEXT_MESSAGE", "ENTERED " + number);
             do {
-                if(cr.getString(cr.getColumnIndex("number")).equals(number)) {
-                    Log.d("TEXT_MESSAGE", "EXISTS");
-                    updateQuery = "UPDATE contacts SET sent = sent + 1, updated_on = current_timestamp WHERE number = " + number;
-                    db.execSQL(updateQuery);
-                    checker = true;
-                }
-            } while (cr.moveToNext());
-            if (!checker) {
-                Log.d("TEXT_MESSAGE", "DOES NOT EXIST");
-                insertQuery = "INSERT INTO contacts (number, sent, received) VALUES (" + number + ", 1, 0);";
+                Log.d("TEXT_MESSAGE", "EXISTS");
+                int id = cr.getInt(cr.getColumnIndex("id"));
+                updateQuery = "UPDATE contacts SET sent = sent + 1, updated_on = current_timestamp WHERE number = " + number + ";";
+                insertQuery = "INSERT INTO sms_sent (contact_id) VALUES (" + id + ");";
+                db.execSQL(updateQuery);
                 db.execSQL(insertQuery);
-            }
+
+            } while (cr.moveToNext());
+            /*if (!checker) {
+                Log.d("TEXT_MESSAGE", "DOES NOT EXIST");
+                insertQuery = "INSERT INTO contacts (number, sent, received) VALUES (" + number + ", 1);";
+                db.execSQL(insertQuery);
+            }*/
             cr.close();
         } else {
             Log.d("TEXT_MESSAGE", "DOES NOT EXIST");
-            insertQuery = "INSERT INTO contacts (number, sent, received) VALUES (" + number + ", 1, 0);";
+            insertQuery = "INSERT INTO contacts (number, sent) VALUES (" + number + ", 1);";
             db.execSQL(insertQuery);
+
+            Cursor c = db.rawQuery(selectQuery, null);
+
+            //boolean checker = false;
+            if (c.moveToFirst()) {
+                do {
+                    Log.d("TEXT_MESSAGE", "EXISTS");
+                    int id = c.getInt(c.getColumnIndex("id"));
+                    insertQuery = "INSERT INTO sms_sent (contact_id) VALUES (" + id + ");";
+                    db.execSQL(insertQuery);
+
+                } while (c.moveToNext());
+                c.close();
+            }
         }
     }
 
