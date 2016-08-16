b81008a2cb5f67ca983216783c7073d52ef86cb9
Santi Gallego <santigallego9@gmail.com>
2016-08-16 01:07:09 -0500

    added more functionality

--- app/src/main/java/com/santigallego/oculytics/SmsBroadcastReceiver.java
+++ app/src/main/java/com/santigallego/oculytics/SmsBroadcastReceiver.java
@@ -3,18 +3,11 @@ package com.santigallego.oculytics;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.provider.Telephony;
-import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
-import android.widget.Toast;
 
-/**
+/*
  * Created by santigallego on 8/14/16.
  */
 public class SmsBroadcastReceiver extends BroadcastReceiver {
@@ -24,9 +17,9 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
                 Telephony.Sms.Intents.getMessagesFromIntent(intent);
         String smsBody = "";
         String address = "";
-        for (int i = 0; i < msgs.length; ++i) {
-            smsBody = msgs[i].getMessageBody().toString();
-            address = msgs[i].getOriginatingAddress();
+        for (SmsMessage msg : msgs) {
+            smsBody = msg.getMessageBody();
+            address = msg.getOriginatingAddress();
         }
         String msgText = "FROM: " + address + "\nMESSAGE: " + smsBody;
         Database.messageReceived(context, address);
