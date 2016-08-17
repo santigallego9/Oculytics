9dc700e717eabbea7237e38a2040acff78a8367d
Santi Gallego <santigallego9@gmail.com>
2016-08-17 16:26:59 -0500

    testing for dates, minor tweaks

--- app/src/main/java/com/santigallego/oculytics/MainActivity.java
+++ app/src/main/java/com/santigallego/oculytics/MainActivity.java
@@ -1,15 +1,10 @@
 package com.santigallego.oculytics;
 
 import android.Manifest;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -18,25 +13,28 @@ import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
-import com.santigallego.oculytics.SmsOutgoingObserver;
 
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
-    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
-    final private int REQUEST_CODE_ASK_PERMISSIONS_TWO = 321;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        smsPermissions();
-        contactPermissions();
+        permissions();
 
         Intent intent = new Intent(this, SmsOutgoingObserver.class);
         startService(intent);
+
+        //DateTime
 
         try {
             SQLiteDatabase db = this.openOrCreateDatabase(Database.DATABASE_NAME, MainActivity.MODE_PRIVATE, null);
@@ -56,73 +54,57 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    private void smsPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS},
-                    REQUEST_CODE_ASK_PERMISSIONS);
+    private  boolean permissions() {
+        int permissionSendMessage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+        int contactPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        int storagePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int phonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+        int internetPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int wakelockPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WAKE_LOCK);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (contactPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_CONTACTS);
+        }
+        if (permissionSendMessage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.SEND_SMS);
+        }
+        if (storagePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (phonePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CALL_PHONE);
+        }
+        if (internetPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.INTERNET);
+        }
+        if (wakelockPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WAKE_LOCK);
         }
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECEIVE_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.RECEIVE_SMS},
-                    REQUEST_CODE_ASK_PERMISSIONS);
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
         }
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_SMS},
-                    REQUEST_CODE_ASK_PERMISSIONS);
-        }
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECEIVE_MMS)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.RECEIVE_MMS},
-                    REQUEST_CODE_ASK_PERMISSIONS);
-        }
+        return true;
     }
-
-    private void contactPermissions() {
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_CONTACTS)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_CONTACTS},
-                    REQUEST_CODE_ASK_PERMISSIONS_TWO);
-
-        }
-    }
-
 
     public void getCount(View view) {
         SQLiteDatabase db = this.openOrCreateDatabase(Database.DATABASE_NAME, MainActivity.MODE_PRIVATE, null);
 
         // UPDATE totals SET received = received + 1
-        String query = "SELECT received FROM totals;";
-        String totals = "", contacts = "", sms_received = "";
+        String query = "SELECT * FROM totals;";
+        String totals = "", contacts = "", sms_received = "", sms_sent = "";
 
         Cursor cr = db.rawQuery(query, null);
 
         if (cr.moveToFirst()) {
             do {
-                int total = cr.getInt(cr.getColumnIndex("received"));
-                Log.d("TEXT_MESSAGE", total + "");
-                totals = "TOTAL: " + total;
+                int total_sent = cr.getInt(cr.getColumnIndex("sent"));
+                int total_received = cr.getInt(cr.getColumnIndex("received"));
+                Log.d("TEXT_MESSAGE", "RECEIVED: " + total_received +  ", SENT: " + total_sent);
+                totals = "RECEIVED: " + total_received + "\nSENT: " + total_sent;
             } while (cr.moveToNext());
             cr.close();
         }
@@ -160,12 +142,63 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("TEXT_MESSAGE", text);
             } while (cr.moveToNext());
             cr.close();
-
-            String final_text = "TOTALS\n------------------\n" + totals + "\n\n\n\nCONTACTS\n------------------\n" + contacts + "\n\n\n\nSMS\n------------------\n" + sms_received;
-
-            TextView queryView = (TextView) findViewById(R.id.query_text);
-
-            queryView.setText(final_text);
         }
+
+        query = "SELECT * FROM sms_sent;";
+
+        cr = db.rawQuery(query, null);
+
+        if (cr.moveToFirst()) {
+            do {
+                int id = cr.getInt(cr.getColumnIndex("id"));
+                int contact_id = cr.getInt(cr.getColumnIndex("contact_id"));
+                String sent_on = cr.getString(cr.getColumnIndex("sent_on"));
+                String text = "ID: " + id + ", CONTACT_ID: " + contact_id + ", SENT_ON: " + sent_on;
+                sms_sent += text + "\n\n";
+                Log.d("TEXT_MESSAGE", text);
+            } while (cr.moveToNext());
+            cr.close();
+        }
+
+        String final_text = "TOTALS\n------------------\n" + totals + "\n\n\n\nCONTACTS\n------------------\n" +
+                contacts + "\n\n\n\nSMS RECEIVED\n------------------\n" +
+                sms_received  + "\n\n\n\nSMS SENT\n------------------\n" + sms_sent;
+
+        TextView queryView = (TextView) findViewById(R.id.query_text);
+
+        queryView.setText(final_text);
+    }
+
+    public void sendText(View view) {
+        SmsManager mgr = SmsManager.getDefault();
+
+        mgr.sendTextMessage("+12543969600", null,
+                "TEST TEXT", null, null);
+    }
+
+    public void datetimeClick(View view) {
+
+        DateTimeFormatter dtfOut = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+        SQLiteDatabase db = this.openOrCreateDatabase(Database.DATABASE_NAME, MainActivity.MODE_PRIVATE, null);
+
+        String datetime = Dates.timeAgo(0, 0, 0, 2, 0, 0);
+        String query = "SELECT * FROM sms_sent WHERE sent_on <= '" + datetime + "';";
+
+        Cursor cr = db.rawQuery(query, null);
+
+        String sent_on = "";
+
+        if (cr.moveToFirst()) {
+            do {
+                sent_on += cr.getString(cr.getColumnIndex("sent_on")) + "\n";
+            } while (cr.moveToNext());
+            cr.close();
+        }
+
+        TextView textView = (TextView) findViewById(R.id.query_text);
+
+        textView.setText(sent_on);
+
     }
 }
