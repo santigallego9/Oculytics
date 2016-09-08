cf6567c1a537588e9f9ab3a88464d0d945cd85a0
Santi Gallego <santigallego9@gmail.com>
2016-09-08 11:07:42 -0500

    battery state and level recievers (in progress)

---
+++ app/src/main/java/com/santigallego/oculytics/services/BatteryLevelReceiver.java
@@ -0,0 +1,20 @@
+package com.santigallego.oculytics.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+import android.util.Log;
+
+import com.santigallego.oculytics.R;
+import com.santigallego.oculytics.activities.MainActivity;
+
+/*
+ * Created by santigallego on 9/7/16.
+ */
+public class BatteryLevelReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/com/santigallego/oculytics/services/PowerConnectionReceiver.java
@@ -0,0 +1,30 @@
+package com.santigallego.oculytics.services;
+
+import android.app.ActionBar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.drawable.ColorDrawable;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.santigallego.oculytics.R;
+import com.santigallego.oculytics.activities.MainActivity;
+
+import java.lang.reflect.Method;
+
+/*
+ * Created by santigallego on 9/7/16.
+ */
+public class PowerConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
+
