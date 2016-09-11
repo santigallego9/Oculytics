0cf9aef02b4492581dfef46de6417ca21272458a
Santi Gallego <santigallego9@gmail.com>
2016-09-11 14:46:11 -0500

    will be removed in the future, being used for debugging purposes

---
+++ app/src/main/java/com/santigallego/oculytics/activities/FileInfoActivity.java
@@ -0,0 +1,62 @@
+package com.santigallego.oculytics.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.santigallego.oculytics.R;
+import com.santigallego.oculytics.helpers.FileUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.jar.Attributes;
+
+public class FileInfoActivity extends AppCompatActivity {
+
+    private static final int FILE_SELECT_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Upload");
+
+        if(FileUtils.isExternalStorageWritable()) {
+            FileUtils.writeToFile("oculytics_test_new", this);
+        }
+
+
+        FileUtils.showFileChooser(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    // Log.d("UPLOAD", "File Uri: " + uri.toString());
+                    // Get the path
+                    String path = FileUtils.getPath(this, uri);
+                    // Log.d("UPLOAD", "File Path: " + path);
+                    // Get the file instance
+                    // File file = new File(path);
+                    // Initiate the upload
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
